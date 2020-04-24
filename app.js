f124c6b2b5024b8849752c58c2718e7c8de0a970
asifurrehman <ex.asif@gmail.com>
2020-04-25 03:59:59 +0600

    Making Initial Score 0 for everybody and Insert a callback funtion

--- app.js
+++ app.js
@@ -11,9 +11,46 @@ GAME RULES:
 
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore=0;
-dice = Math.floor(Math.random()*6)+1;
+activePlayer=1;
 
+document.querySelector('.dice').style.display ='none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    // 1.Generate a random number 
+   var dice = Math.floor(Math.random()*6)+1;
+   console.log(dice);
+
+   // 2. Display the Result
+   var diceDom = document.querySelector('.dice'); 
+   diceDom.style.display = 'block';
+   diceDom.src ='dice-'+dice+'.png';
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.querySelector('#score-'+activePlayer).textContent = dice;
+
+//document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' +dice + '</em>';
